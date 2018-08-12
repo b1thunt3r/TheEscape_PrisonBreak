7bea64db5e8116d88a22964662048a31fdb498ef
Ishan Jain <ishan@jain.se>
2018-08-12 11:28:44 +0100

    Created a list of standard colors

---
+++ Assets/Scripts/Colors/ColorDefinationAttribute.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Reflection;
+using UnityEngine;
+
+public class ColorDefinationAttribute : Attribute
+{
+    public Single Red { get; set; }
+    public Single Green { get; set; }
+    public Single Blue { get; set; }
+    public Single Alpha { get; set; }
+
+    public ColorDefinationAttribute(float red, float green, float blue, float alpha)
+    {
+        Red = red;
+        Green = green;
+        Blue = blue;
+        Alpha = alpha;
+    }
+
+    public ColorDefinationAttribute(float red, float green, float blue)
+        : this (red, green, blue, 1f)
+    { }
+
+}
+
+public static class StandardColorsColor
+{
+    public static Color GetColor<T>(this T source) where T : IConvertible
+    {
+        FieldInfo fi = source.GetType().GetField(source.ToString());
+
+        var attributes = fi.GetCustomAttributes(typeof(ColorDefinationAttribute), false) as ColorDefinationAttribute[];
+
+        if (attributes != null && attributes.Length > 0)
+        {
+            var c = attributes[0];
+            var color = new Color(c.Red, c.Green, c.Blue, c.Alpha);
+            return color;
+        }
+        else
+        {
+            return new Color(0, 0, 0, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/Colors/StandardColors.cs
@@ -0,0 +1,10 @@
+﻿public enum StandardColor
+{
+    [ColorDefination(000 / 255f, 000 / 255f, 000 / 255f, 0f)] None,
+    [ColorDefination(192 / 255f, 000 / 255f, 000 / 255f)] Red,
+    [ColorDefination(000 / 255f, 111 / 225f, 192 / 255f)] Blue,
+    [ColorDefination(255 / 255f, 217 / 255f, 100 / 255f)] Yellow,
+    [ColorDefination(120 / 255f, 147 / 255f, 064 / 255f)] Green,
+    [ColorDefination(000 / 255f, 000 / 255f, 000 / 255f)] Black,
+    [ColorDefination(255 / 255f, 255 / 255f, 255 / 255f)] White,
+}
